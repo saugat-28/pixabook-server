0bcc7265d8e22a9d81fe86a256ede553b395dca2
saugat-28 <55578635+saugat-28@users.noreply.github.com>
2022-12-02 19:26:47 +0530

    Changed port

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ app.use('/user', userRoutes);
 // Set Port and Connect to Mongoose database
 const PORT = process.env.PORT 
 
-mongoose.connect(process.env.LOCAL_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server running on Port: ${PORT}`)))
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT || 5000, () => console.log(`Server running on Port: ${PORT}`)))
     .catch((error) => console.log(error.message));
